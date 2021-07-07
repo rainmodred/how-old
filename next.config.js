6c150b9fef633c26b2ad54e3a8bdf7ffc48c70ef
rainmodred <albphoenix@gmail.com>
2021-07-07 14:25:57 +0300

    add env file

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
+
 module.exports = {
   reactStrictMode: true,
   env: {
-    apiKey: '2dd64aa53d6983b5dbdf98bb6d2b294e',
+    apiKey: process.env.API_KEY,
   },
   images: {
     domains: ['image.tmdb.org'],
