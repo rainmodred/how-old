9129c96b5fe3712a0ea3c5b595453e6738b86873
rainmodred <albphoenix@gmail.com>
2021-07-17 19:39:27 +0300

    refactor: tvShows

--- src/pages/tv/[id].js
+++ src/pages/tv/[id].js
@@ -3,35 +3,19 @@ import Head from 'next/head';
 import { Heading, Select } from '@chakra-ui/react';
 
 import Layout from '@/components/Layout';
-import { useTvShow, useTvShowCast } from '@/hooks/swr';
+import { useTvShowCast } from '@/hooks/swr';
 import Persons from '@/components/Persons';
-
-function getSeasonAirDate(seasons, season = 1) {
-  return seasons?.find(({ season_number }) => season_number === Number(season))
-    ?.air_date;
-}
 
 export default function TvShow() {
   const router = useRouter();
-
   const { id, season, title } = router.query;
-
-  const { tvShow } = useTvShow(id);
-
-  const { cast, isLoading } = useTvShowCast(
-    id,
-    getSeasonAirDate(tvShow?.seasons, season),
-    season,
-  );
+  const { cast, seasons, isLoading } = useTvShowCast(id, season);
 
   function handleSelect(e) {
     if (e.target.value !== '') {
       router.push(`/tv/${id}?season=${e.target.value}`);
     }
   }
-
-  const seasons =
-    tvShow?.seasons.filter(season => season.season_number > 0) || [];
 
   return (
     <>
@@ -44,21 +28,22 @@ export default function TvShow() {
         </Heading>
 
         <Select onChange={handleSelect} placeholder="Select season">
-          {seasons.map(({ id, season_number, name }) => {
-            if (season_number === Number(season)) {
+          {!isLoading &&
+            seasons.map(({ id, season_number, name }) => {
+              if (season_number === Number(season)) {
+                return (
+                  <option key={id} value={season_number} selected>
+                    {name}
+                  </option>
+                );
+              }
+
               return (
-                <option key={id} value={season_number} selected>
+                <option key={id} value={season_number}>
                   {name}
                 </option>
               );
-            }
-
-            return (
-              <option key={id} value={season_number}>
-                {name}
-              </option>
-            );
-          })}
+            })}
         </Select>
 
         <Persons persons={cast} isLoading={isLoading} />
