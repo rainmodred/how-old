27d823687905493bd5e860c70cf135e919f49dd2
rainmodred <albphoenix@gmail.com>
2022-01-31 00:00:55 +0300

    feat: change design

--- src/components/Person.js
+++ src/components/Person.js
@@ -1,4 +1,4 @@
-import { Flex, Text, Box } from '@chakra-ui/react';
+import { Flex, Text, Box, Tr, Td } from '@chakra-ui/react';
 import NextImage from 'next/image';
 import profileFallback from '../../public/profileFallback.svg';
 
@@ -32,53 +32,35 @@ export default function Person({ person }) {
     : null;
 
   return (
-    <Box p="1" w="100%" borderRadius="10px">
-      <Flex alignItems="center">
-        <ChakraNextImage
-          h={{ base: '103px', md: '120px' }}
-          w={{ base: '62px', md: '72px' }}
-          minW="62px"
-          src={imgSrc}
-          alt={`${name} image`}
-          borderRadius="10px"
-          overflow="hidden"
-        ></ChakraNextImage>
-        <Box p="4">
-          <Text
-            whiteSpace={['wrap', 'nowrap']}
-            fontWeight="bold"
-            data-testid="name"
-          >
-            {name} / {character}
-          </Text>
-          <Text whiteSpace="nowrap" data-testid="birthday">
-            <Text as="b">Born: </Text>
-            {birthday}
-          </Text>
-          {deathday && (
-            <Text whiteSpace="nowrap" data-testid="deathday">
-              <Text as="b">Died: </Text>
-              {deathday}
+    <Tr>
+      <Td>
+        <Flex alignItems="center" gridGap="2">
+          <ChakraNextImage
+            h={{ base: '103px', md: '120px' }}
+            w={{ base: '30px', md: '72px' }}
+            minW="62px"
+            src={imgSrc}
+            alt={`${name} image`}
+            borderRadius="10px"
+            overflow="hidden"
+          ></ChakraNextImage>
+          <Box>
+            <Text
+              whiteSpace={['wrap', 'nowrap']}
+              fontWeight="bold"
+              data-testid="name"
+            >
+              {name}
             </Text>
-          )}
-
-          <Text whiteSpace="nowrap" data-testid="age">
-            <Text as="b">{age}</Text> years old
-          </Text>
-        </Box>
-        <Box ml="auto">
-          <Box
-            p="15px"
-            borderRadius="50%"
-            bg="teal.400"
-            color="white"
-            fontWeight="bold"
-            data-testid="ageOnRelease"
-          >
-            {ageOnRelease}
+            <Text>{character}</Text>
+            <Text>Birthday: {birthday}</Text>
           </Box>
-        </Box>
-      </Flex>
-    </Box>
+        </Flex>
+      </Td>
+      <Td isNumeric>{ageOnRelease}</Td>
+      <Td isNumeric textAlign="center">
+        <Text>{deathday ? ` ${deathday} (${age})` : age}</Text>
+      </Td>
+    </Tr>
   );
 }
--- src/components/Persons.js
+++ src/components/Persons.js
@@ -1,15 +1,28 @@
-import { VStack, Spinner } from '@chakra-ui/react';
+import { Spinner, Table, Thead, Th, Tr, Tbody } from '@chakra-ui/react';
 
 import Person from '@/components/Person';
 
 export default function Persons({ persons, isLoading }) {
   return (
-    <VStack mt="10" w="100%" spacing="0">
+    <>
       {isLoading ? (
         <Spinner size="lg" />
       ) : (
-        persons.map(person => <Person key={person.id} person={person} />)
+        <Table size="sm">
+          <Thead>
+            <Tr>
+              <Th>Actor</Th>
+              <Th isNumeric>Age then</Th>
+              <Th isNumeric>Age now</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {persons.map(person => (
+              <Person key={person.id} person={person} />
+            ))}
+          </Tbody>
+        </Table>
       )}
-    </VStack>
+    </>
   );
 }
