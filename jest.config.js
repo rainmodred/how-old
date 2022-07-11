16d5d329a403a83cb97701f232471960ed8678b8
rainmodred <albphoenix@gmail.com>
2022-07-12 02:13:22 +0300

    chore: update dependencies

--- jest.config.js
+++ jest.config.js
@@ -3,23 +3,44 @@
  * https://jestjs.io/docs/configuration
  */
 
-module.exports = {
-  clearMocks: true,
-  collectCoverage: true,
-  collectCoverageFrom: [
-    'src/**/*.js',
-    '!src/mocks/**/*.js',
-    '!src/pages/**/*.js',
-  ],
-  coverageDirectory: 'coverage',
-  coverageProvider: 'v8',
-  testEnvironment: 'jsdom',
-  transform: {
-    '^.+\\.(js|jsx)$': 'babel-jest',
-    '.+\\.(svg)$': 'jest-transform-stub',
-  },
+// module.exports = {
+//   clearMocks: true,
+//   collectCoverage: true,
+//   collectCoverageFrom: [
+//     'src/**/*.js',
+//     '!src/mocks/**/*.js',
+//     '!src/pages/**/*.js',
+//   ],
+//   coverageDirectory: 'coverage',
+//   coverageProvider: 'v8',
+//   testEnvironment: 'jsdom',
+//   transform: {
+//     '^.+\\.(js|jsx)$': 'babel-jest',
+//     '.+\\.(svg)$': 'jest-transform-stub',
+//   },
+//   setupFiles: [require.resolve('whatwg-fetch')],
+//   setupFilesAfterEnv: ['./setupTests.js'],
+//   moduleNameMapper: {
+//     '^@/components/(.*)$': '<rootDir>/src/components/$1',
+//     '^@/pages/(.*)$': '<rootDir>/src/pages/$1',
+//     '^@/hooks/(.*)$': '<rootDir>/src/hooks/$1',
+//     '^@/utils/(.*)$': '<rootDir>/src/utils/$1',
+//     '^@/mocks/(.*)$': '<rootDir>/src/mocks/$1',
+//   },
+// };
+
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+// Add any custom config to be passed to Jest
+const customJestConfig = {
   setupFiles: [require.resolve('whatwg-fetch')],
   setupFilesAfterEnv: ['./setupTests.js'],
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  testEnvironment: 'jest-environment-jsdom',
   moduleNameMapper: {
     '^@/components/(.*)$': '<rootDir>/src/components/$1',
     '^@/pages/(.*)$': '<rootDir>/src/pages/$1',
@@ -28,3 +49,5 @@ module.exports = {
     '^@/mocks/(.*)$': '<rootDir>/src/mocks/$1',
   },
 };
+
+module.exports = createJestConfig(customJestConfig);
