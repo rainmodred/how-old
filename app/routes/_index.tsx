fcdd3ac0a45db52a09b409d94a31727829324209
rainmodred <albphoenix@gmail.com>
2023-12-24 23:20:47 +0300

    init

---
+++ app/routes/_index.tsx
@@ -0,0 +1,17 @@
+import type { MetaFunction } from '@remix-run/node';
+import { ColorSchemeToggle } from '~/components/ColorSchemeToggle/ColorSchemeToggle';
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: 'Mantine Remix App' },
+    { name: 'description', content: 'Welcome to Mantine!' },
+  ];
+};
+
+export default function Index() {
+  return (
+    <div>
+      <ColorSchemeToggle />
+    </div>
+  );
+}
