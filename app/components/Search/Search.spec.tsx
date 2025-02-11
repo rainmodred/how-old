df31f8313d8694fe31ded8c2958e22f67d87909f
rainmodred <albphoenix@gmail.com>
2025-02-11 17:02:02 +0300

    Fix tests

--- app/components/Search/Search.spec.tsx
+++ app/components/Search/Search.spec.tsx
@@ -11,8 +11,11 @@ import { loader } from '../../routes/action.search';
 import userEvent from '@testing-library/user-event';
 import { MantineProvider } from '@mantine/core';
 import { Outlet } from '@remix-run/react';
+import { db } from 'tests/mocks/db';
+import { getYear } from 'date-fns';
 
 it('should search', async () => {
+  const testMovie = db.movie.getAll()[0];
   const user = userEvent.setup();
   const RemixStub = createRemixStub([
     {
@@ -44,14 +47,15 @@ it('should search', async () => {
 
   await waitFor(() => screen.getByRole('searchbox'));
   const searchbox = screen.getByRole('searchbox');
-  await user.type(searchbox, 'the');
+  await user.type(searchbox, testMovie.title.slice(0, 4));
 
   const loading = await screen.findByTestId('loading');
   expect(loading).toBeInTheDocument();
   await waitForElementToBeRemoved(loading);
 
+  const title = `${testMovie.title} (${getYear(testMovie.release_date)})`;
   const movie = screen.getByRole('option', {
-    name: /the lord of the rings: the fellowship of the ring \(2001\)/i,
+    name: title,
   });
   expect(movie).toBeInTheDocument();
   await user.click(movie);
