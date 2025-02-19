1dbced8856f3a3fc7daec5b0b66623b86d7bbb41
rainmodred <albphoenix@gmail.com>
2025-02-19 16:42:15 +0300

    Fix error boundary

--- app/root.tsx
+++ app/root.tsx
@@ -8,8 +8,6 @@ import {
   ScrollRestoration,
   Scripts,
   useNavigation,
-  isRouteErrorResponse,
-  useRouteError,
 } from 'react-router';
 import {
   Text,
@@ -96,36 +94,10 @@ export default function App() {
   );
 }
 
-// export function ErrorBoundary() {
-//   return (
-//     <Document>
-//       <ServerError />
-//     </Document>
-//   );
-// }
-
 export function ErrorBoundary() {
-  const error = useRouteError();
-
-  if (isRouteErrorResponse(error)) {
-    return (
-      <Document>
-        <h1>
-          {error.status} {error.statusText}
-        </h1>
-        <p>{error.data}</p>
-      </Document>
-    );
-  } else if (error instanceof Error) {
-    return (
-      <Document>
-        <h1>Error</h1>
-        <p>{error.message}</p>
-        <p>The stack trace is:</p>
-        <pre>{error.stack}</pre>
-      </Document>
-    );
-  } else {
-    return <h1>Unknown Error</h1>;
-  }
+  return (
+    <Document>
+      <ServerError />
+    </Document>
+  );
 }
